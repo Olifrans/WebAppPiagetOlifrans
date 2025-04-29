11b4d8cc09b5f3358939f5907a36b22139c6b051
Aluno <Aluno@UPGT-SPBR-PC146>
2025-04-29 10:07:09 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppPiagetOlifrans/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppPiagetOlifrans.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
